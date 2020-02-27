ba47513e8aabdb63dbb34a68862006c2e4adfb39
bucai <1450941858@qq.com>
2020-02-27 08:47:34 +0800

    update

--- middles/black_user.js
+++ middles/black_user.js
@@ -3,15 +3,15 @@ module.exports = async function (loginName, store) {
   const key = loginName + '_black';
   const userLoginNum = (await store.get(key)) || { num: 0, time: 0 };
 
-  if (userLoginNum.num >= 80) {
+  if (userLoginNum.num >= 90) {
     throw {
       code: -1,
       msg: "兄弟，登陆这么多次不累吗，服务器好累的"
     }
   }
 
-  // 10秒的登陆间隙
-  if (userLoginNum.time + 5000 > Date.now()) {
+  // 登陆间隙
+  if (userLoginNum.time + 2000 > Date.now()) {
     throw {
       code: -1,
       msg: "登陆频繁，5秒后再试试吧"
